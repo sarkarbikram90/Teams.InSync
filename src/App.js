1a8dc3a56f7fa3f4adca3f07e4d1f8ecb759436f
Bikram Sarkar <sarkarbikram90@gmail.com>
2024-03-24 09:07:31 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
-import Video from './Video'
-import Home from './Home'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import Video from './Video';
+import Home from './Home';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 class App extends Component {
-	render() {
-		return (
-			<div>
-				<Router>
-					<Switch>
-						<Route path="/" exact component={Home} />
-						<Route path="/:url" component={Video} />
-					</Switch>
-				</Router>
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/:url" component={Video} />
+                        <Redirect to="/" />
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
-
